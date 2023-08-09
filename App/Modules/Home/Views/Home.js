75a9530cf625c3eec8d67ef54a5996ac9862b9fa
Eduardito187 <eduardchavez302@gmail.com>
2023-08-09 02:01:24 -0400

    enable expo update

--- App/Modules/Home/Views/Home.js
+++ App/Modules/Home/Views/Home.js
@@ -44,6 +44,7 @@ import ModalDisableComponen from './Components/ModalDisable';
 import ModalCloseComponen from './Components/ModalClose';
 import ModalVersionComponen from './Components/ModalVersion';
 import { getTokenNotification, settingToken } from '../../../Helpers/Code';
+import * as Updates from 'expo-updates';
 
 const Home = ({ route, navigation }) => {
   const [heightBar, SetHeightBar] = React.useState(getStatusBarHeight());
@@ -66,10 +67,23 @@ const Home = ({ route, navigation }) => {
     setToken();
   }, []);
 
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      //
+    }
+  }
   async function backgroundFetchTask(TOKEN_SESSIONS){
     try {
       const token_expo = await getTokenNotification(TOKEN_SESSIONS);
       await settingToken(TOKEN_SESSIONS, token_expo);
+      onFetchUpdateAsync();
     } catch (error) {
       //
     }
